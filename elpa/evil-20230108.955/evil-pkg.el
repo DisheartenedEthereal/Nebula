d844d9cca60d5d846ddbc22d9639d69f24f793fb
Ethereal <dethereal@protonmail.com>
2023-01-08 18:53:55 +0330

    New branch, added evil mode

---
+++ elpa/evil-20230108.955/evil-pkg.el
@@ -0,0 +1,12 @@
+(define-package "evil" "20230108.955" "Extensible Vi layer for Emacs."
+  '((emacs "24.1")
+    (goto-chg "1.6")
+    (cl-lib "0.5"))
+  :commit "e324f8daf152301fd59b045da2331da7497aaec4" :maintainer
+  '("Tom Dalziel" . "tom.dalziel@gmail.com")
+  :keywords
+  '("emulations")
+  :url "https://github.com/emacs-evil/evil")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
